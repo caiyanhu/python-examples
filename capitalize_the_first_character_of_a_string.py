8559cb70e820aecba8e4c40441ccb4c4765b423a
caiyanhu <caiyanhu@cmss.chinamobile.com>
2025-12-17 14:09:42 +0800

    feat: Day09
    Code Source From: Self Code
    Description:
    Jira:
    市场项目编号（名称）:

---
+++ capitalize_the_first_character_of_a_string.py
@@ -0,0 +1,6 @@
+my_string = "programiz is Lit"
+print(my_string[0].upper() + my_string[1:])
+
+my_string = "programiz is Lit"
+# capitalize() changes the first character to uppercase; however, changes all other characters to lowercase.
+print(my_string.capitalize())
---
+++ convert_bytes_to_a_string.py
@@ -0,0 +1,3 @@
+print(b"Easy \xe2\x9c\x85".decode("utf-8"))
+emoji = "✅"
+print(emoji.encode())
